1af417257299bf49c28d183921f1d61f52be0e26
ISAWASHUN <syun20000616@icloud.com>
2023-06-18 13:00:33 +0900

    Taskの一覧機能作成

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import Task from "./components/Task";
+import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
+import axios from "axios";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const fetch = async () => {
+    const res = await axios.get("http://localhost:3010/tasks")
+    console.log(res)
+    setTasks(res.data)
+  }
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
+  const toggleIsDone = (index) => {
+    const tasksCopy = [...tasks]
+    const isDone = tasksCopy[index].isDone
+    tasksCopy[index].isDone = !isDone
+    setTasks(tasksCopy)
+  }
+
+  return (
+    <Box mt="64px">
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            {tasks.map((task, index) => {
+              return (
+                <Task 
+                  key={index}
+                  index={index}
+                  name={task.name}
+                  isDone={task.isDone}
+                  toggleIsDone={toggleIsDone}
+                />
+              )
+            })}
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/components/Task.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Checkbox, Box, Text } from "@chakra-ui/react";
+
+const Task = (props) => {
+  return (
+    <Box mb="16px">
+      <Checkbox 
+      colorScheme="blue" 
+      size="lg"
+      isChecked={props.isDone}
+      onChange={() => {props.toggleIsDone(props.index)}}
+      >
+        <Text>{props.name}</Text>
+      </Checkbox>
+    </Box>
+  )
+}
+
+export default Task
